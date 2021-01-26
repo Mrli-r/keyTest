ef28209a8bfec80677ddd838d2f7073a7b33f8d2
lizhe <lizhe34187@hundsun.com>
2021-01-26 10:32:42 +0800

    1.0.0分支

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,9 @@
+import net.minidev.json.JSONUtil;
+import org.w3c.dom.ls.LSOutput;
+
+public class PositionTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,word");
+    }
+}
